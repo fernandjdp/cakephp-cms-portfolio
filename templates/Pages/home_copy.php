68e7526a5d436ce4fe6c7cf9bbae1826df810381
Fernand Jose Diaz Perez <fernand.diaz1708@gmail.com>
2021-11-15 17:50:31 -0400

    DELETE: Eliminadas las dependencias de tailwindcss

--- templates/Pages/home_copy.php
+++ templates/Pages/home_copy.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link      https://cakephp.org CakePHP(tm) Project
+ * @since     0.10.0
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
+ * @var \App\View\AppView $this
+ */
 use Cake\Cache\Cache;
 use Cake\Core\Configure;
 use Cake\Core\Plugin;
@@ -27,7 +41,13 @@ $checkConnection = function (string $name) {
     return compact('connected', 'error');
 };
 
-$title = 'Fernand Diaz - Portfolio';
+if (!Configure::read('debug')) :
+    throw new NotFoundException(
+        'Please replace templates/Pages/home.php with your own version or re-enable debug mode.'
+    );
+endif;
+
+$cakeDescription = 'CakePHP: the rapid development PHP framework';
 ?>
 <!DOCTYPE html>
 <html>
@@ -35,7 +55,7 @@ $title = 'Fernand Diaz - Portfolio';
     <?= $this->Html->charset() ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>
-        <?= $title ?>:
+        <?= $cakeDescription ?>:
         <?= $this->fetch('title') ?>
     </title>
     <?= $this->Html->meta('icon') ?>
