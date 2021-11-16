d2c2e0c4d9e819788d189e25e59b54cb94e0bf88
Fernand Jose Diaz Perez <fernand.diaz1708@gmail.com>
2021-11-15 20:17:32 -0400

    FIX: Modificacion de tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      sky: colors.sky
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
